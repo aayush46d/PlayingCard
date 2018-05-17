554e3c96158ba593aecd53fee551a519cefad63d
Aayush Srivastava <aayush46d@gmail.com>
2018-05-17 15:48:55 -0400

    experiment with UIBezier path

--- PlayingCards/PlayingCardView.swift
+++ PlayingCards/PlayingCardView.swift
@@ -11,13 +11,21 @@ import UIKit
 class PlayingCardView: UIView {
 
     override func draw(_ rect: CGRect) {
-        if let context = UIGraphicsGetCurrentContext(){
-            context.addArc(center: CGPoint(x: bounds.midX, y: bounds.midY), radius: 100, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
-            context.setLineWidth(5.0)
-            UIColor.blue.setStroke()
-            UIColor.red.setFill()
-            context.strokePath()
-        }
+//        if let context = UIGraphicsGetCurrentContext(){
+//            context.addArc(center: CGPoint(x: bounds.midX, y: bounds.midY), radius: 100, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
+//            context.setLineWidth(5.0)
+//            UIColor.blue.setStroke()
+//            UIColor.red.setFill()
+//            context.fillPath()
+//        }
+        
+        let path = UIBezierPath()
+        path.addArc(withCenter: CGPoint(x: bounds.midX, y: bounds.midY), radius: 100, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
+        path.lineWidth = 5.0
+        #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1).setFill()
+        #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1).setStroke()
+        path.stroke()
+        path.fill()
         
     }
 
